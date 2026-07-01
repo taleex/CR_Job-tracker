e270e5732025e90c9ef9deb097a74a1e4ed4813f
Taleex <matosjax@gmail.com>
2026-07-01 11:36:33 +0100

    GlobalCSS Redesign

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,13 +1,17 @@
+import FooterSection from "../components/HomePage/FooterSection";
 import HeroSection from "../components/HomePage/HeroSection/heroSection";
 import NavBar from "../components/HomePage/navBarSection/navBar";
 import PlanSection from "../components/HomePage/PlanSection/PlanSection";
 
 export default function HomePage() {
   return (
-    <div>
+    <main className="HomePage">
       <NavBar />
-      <HeroSection />
-      <PlanSection />
-    </div>
+      <div className="HomePageContent">
+        <HeroSection />
+        <PlanSection />
+      </div>
+      <FooterSection />
+    </main>
   )
 }
---
+++ app/components/HomePage/FooterSection.tsx
@@ -0,0 +1,12 @@
+import Logo from "../Logo";
+
+export default function FooterSection() {
+  return (
+    <footer className="footer">
+        <section>
+            <Logo />
+        </section>
+        <p>&copy; 2024 Job Tracker. All rights reserved.</p>
+    </footer>
+  )
+}
